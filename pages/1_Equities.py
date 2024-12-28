05328d3dbb4a4a024caff785052d1588a7fd1a82
arish-pitchai_pwcit <arish.pitchai@pwc.com>
2024-12-29 03:36:29 +0530

    previous close added

--- pages/1_Equities.py
+++ pages/1_Equities.py
@@ -12,8 +12,8 @@ def initialize_session_state():
     if 'total_invested_value_equity' not in st.session_state:
         st.session_state['total_invested_value_equity'] = 0.0
     if 'equity_column_headers' not in st.session_state:
-        st.session_state['equity_column_headers'] = ['Stock name', 'No. of stocks', 'Invested price', 'Invested value', 'Current price', 
-                                                     'Current value', 'Profit/ Loss value', 'Profit/ Loss %']
+        st.session_state['equity_column_headers'] = ['Stock name', 'No. of stocks', 'Invested price', 'Current price', 
+                                                     'Profit/ Loss', 'Previous close']
     if 'equity_page_count' not in st.session_state:
         st.session_state['equity_page_count'] = 0
 
@@ -23,7 +23,7 @@ def display_column_header():
         equity_titles = st.columns(len(st.session_state['equity_column_headers']))
         for i in range(len(st.session_state['equity_column_headers'])):
             with equity_titles[i]:
-                st.text(st.session_state['equity_column_headers'][i])
+                st.write(f"**{st.session_state['equity_column_headers'][i]}**")
 
 def load_equity_page():
     st.session_state['total_invested_value_equity'] = 0.0
@@ -33,7 +33,7 @@ def load_equity_page():
         portfolio_data = json.load(file)
     # display details of all stocks        
     try:
-        with stock_values_container.container(border=True, height=400):
+        with stock_values_container.container(border=True, height=600):
             # for each stock in the portfolio
             for row in portfolio_data["equity"]:
                 stock_name = row
@@ -41,7 +41,12 @@ def load_equity_page():
                 invested_price = portfolio_data['equity'][row]["invested price"] # invested price per share of a stock
                 invested_value = n_stocks * invested_price
                 current_price = portfolio_data['equity'][row]["current price"] # current price per share of a stock
+                previous_close = portfolio_data['equity'][row]['previous close']
+                prevdiff_value = current_price - previous_close
+                prevdiff_percent = prevdiff_value/ previous_close * 100.00
+                todaypl_value = prevdiff_value * n_stocks
                 current_value = n_stocks * current_price
+                pl_price = current_price - invested_price
                 pl_value = current_value - invested_value # profit or loss value
                 pl_percent = pl_value/invested_value*100.00
                 st.session_state['total_invested_value_equity'] += invested_value # total invested value
@@ -49,32 +54,38 @@ def load_equity_page():
                 # display values
                 equity_values = st.columns(len(st.session_state['equity_column_headers']))
                 with equity_values[0]:
-                    st.text_input(value=stock_name, key=f'stock_name_{row}_load', label='stock_name', 
-                                  label_visibility='collapsed', disabled=True)
+                    st.write(stock_name)
                 with equity_values[1]:
-                    portfolio_data['equity'][row]["no. of shares"] = st.number_input(value=n_stocks, key=f'n_stocks_{row}_load', 
-                                                                                     label='n_stocks', label_visibility='collapsed')
+                    st.write(f"{n_stocks}")
                 with equity_values[2]:
-                    portfolio_data['equity'][row]["invested price"] = st.number_input(value=invested_price, key=f'inv_price_{row}_load', 
-                                                                                      label='invested_price', label_visibility='collapsed', step=0.01)
+                    st.write(f"Rs. {invested_price}/ share")
+                    txt = "Rs. {:.2f}"
+                    st.write(txt.format(invested_value))
                 with equity_values[3]:
-                    st.number_input(value=invested_value, key=f'inv_value_{row}_load', label='invested_value', 
-                                    label_visibility='collapsed', disabled=True, format="%.2f")
+                    st.write(f"Rs. {current_price}/ share")
+                    txt = "Rs. {:.2f}"
+                    st.write(txt.format(current_value))
                 with equity_values[4]:
-                    portfolio_data['equity'][row]["current price"] = st.number_input(value=current_price, key=f'curr_price_{row}_load', 
-                                                                                    label='current_price', label_visibility='collapsed', 
-                                                                                    disabled=True)
+                    if pl_price > 0.0:
+                        txt_pl_price = ":green[Rs. {:.2f}/ share]"
+                        txt_pl_value = ":green[Rs. {:.2f} ({:.2f}%)]"
+                    else:
+                        txt_pl_price = ":red[Rs. {:.2f}/ share]"
+                        txt_pl_value = ":red[Rs. {:.2f} ({:.2f}%)]"
+                    st.write(txt_pl_price.format(pl_price))
+                    st.write(txt_pl_value.format(pl_value, pl_percent))
                 with equity_values[5]:
-                    st.number_input(value=current_value, key=f'curr_value_{row}_load', label='current_value', 
-                                    label_visibility='collapsed', disabled=True, format="%.2f")
-                with equity_values[6]:
-                    st.number_input(value=pl_value, key=f'profit_loss_{row}_load', label='profit_loss', 
-                                    label_visibility='collapsed', disabled=True, format="%.2f")
-                with equity_values[7]:
-                    st.number_input(value=pl_percent, key=f'pl_percent_{row}_load', label='pl_percent', 
-                                    label_visibility='collapsed', disabled=True, format="%.2f")
-                with open("data/portfolio.json", "w") as file:
-                    json.dump(portfolio_data, file, indent=4)
+                    if prevdiff_value > 0.0:
+                        txt_pl_prev = "Rs. {:.2f} :green[(Rs. {:.2f})]"
+                    else:
+                        txt_pl_prev = "Rs. {:.2f} :red[(Rs. {:.2f})]"
+                    st.write(txt_pl_prev.format(previous_close, prevdiff_value))
+                    if todaypl_value > 0.0:
+                        txt_pl_today = "P/L: :green[Rs. {:.2f} ({:.2f}%)]"
+                    else:
+                        txt_pl_today = "P/L: :red[Rs. {:.2f} ({:.2f}%)]"
+                    st.write(txt_pl_today.format(todaypl_value, prevdiff_percent))
+                st.divider()
     except Exception as e:
         st.write(str(e))
 
@@ -86,7 +97,7 @@ def refresh_equity_values():
         portfolio_data = json.load(file)
     # display details of all stocks        
     try:
-        with stock_values_container.container(border=True, height=400):
+        with stock_values_container.container(border=True, height=600):
             # for each stock in the portfolio
             for row in portfolio_data["equity"]:
                 stock_name = row
@@ -97,7 +108,18 @@ def refresh_equity_values():
                     current_price = yfinance.Ticker(ticker=row).info['currentPrice']
                 except:
                     current_price = yfinance.Ticker(ticker=row).info['previousClose'] # current price per share of a stock
+                try:
+                    previous_close = yfinance.Ticker(ticker=row).info['previousClose']
+                except:
+                    previous_close = current_price
+                portfolio_data['equity'][row]["current price"] = current_price
+                portfolio_data['equity'][row]["previous close"] = previous_close
+                with open("data/portfolio.json", "w") as file:
+                    json.dump(portfolio_data, file, indent=4)
+                diff_value = current_price - previous_close
+                diff_percent = diff_value/ previous_close * 100.00
                 current_value = n_stocks * current_price
+                pl_price = current_price - invested_price
                 pl_value = current_value - invested_value # profit or loss value
                 pl_percent = pl_value/invested_value*100.00
                 st.session_state['total_invested_value_equity'] += invested_value # total invested value
@@ -105,32 +127,36 @@ def refresh_equity_values():
                 # display values
                 equity_values = st.columns(len(st.session_state['equity_column_headers']))
                 with equity_values[0]:
-                    st.text_input(value=stock_name, key=f'stock_name_{row}', label='stock_name', label_visibility='collapsed', disabled=True)
+                    st.write(stock_name)
                 with equity_values[1]:
-                    portfolio_data['equity'][row]["no. of shares"] = st.number_input(value=n_stocks, key=f'n_stocks_{row}', label='n_stocks', 
-                                                                                    label_visibility='collapsed')
+                    st.write(f"{n_stocks}")
                 with equity_values[2]:
-                    portfolio_data['equity'][row]["invested price"] = st.number_input(value=invested_price, key=f'inv_price_{row}', 
-                                                                                    label='invested_price', label_visibility='collapsed', 
-                                                                                    step=0.01)
+                    st.write(f"Rs. {invested_price}/ share")
+                    txt = "Rs. {:.2f}"
+                    st.write(txt.format(invested_value))
                 with equity_values[3]:
-                    st.number_input(value=invested_value, key=f'inv_value_{row}', label='invested_value', label_visibility='collapsed', 
-                                    disabled=True, format="%.2f")
+                    st.write(f"Rs. {current_price}/ share")
+                    txt = "Rs. {:.2f}"
+                    st.write(txt.format(current_value))
                 with equity_values[4]:
-                    portfolio_data['equity'][row]["current price"] = st.number_input(value=current_price, key=f'curr_price_{row}', 
-                                                                                    label='current_price', label_visibility='collapsed', 
-                                                                                    disabled=True)
+                    if pl_price > 0.0:
+                        txt_pl_price = ":green[Rs. {:.2f}/ share]"
+                        txt_pl_value = ":green[Rs. {:.2f} ({:.2f}%)]"
+                    else:
+                        txt_pl_price = ":red[Rs. {:.2f}/ share]"
+                        txt_pl_value = ":red[Rs. {:.2f} ({:.2f}%)]"
+                    st.write(txt_pl_price.format(pl_price))
+                    st.write(txt_pl_value.format(pl_value, pl_percent))
                 with equity_values[5]:
-                    st.number_input(value=current_value, key=f'curr_value_{row}', label='current_value', label_visibility='collapsed', 
-                                    disabled=True, format="%.2f")
-                with equity_values[6]:
-                    st.number_input(value=pl_value, key=f'profit_loss_{row}', label='profit_loss', label_visibility='collapsed', 
-                                    disabled=True, format="%.2f")
-                with equity_values[7]:
-                    st.number_input(value=pl_percent, key=f'pl_percent_{row}', label='pl_percent', label_visibility='collapsed', 
-                                    disabled=True, format="%.2f")
-                with open("data/portfolio.json", "w") as file:
-                    json.dump(portfolio_data, file, indent=4)
+                    st.write(f"Rs. {previous_close}/ share")
+                    diff_value = current_price - previous_close
+                    diff_percent = diff_value/ previous_close
+                    if diff_value > 0.0:
+                        txt_pl_prev = ":green[Rs. {:.2f} ({:.2f}%)]"
+                    else:
+                        txt_pl_prev = ":red[Rs. {:.2f} ({:.2f}%)]"
+                    st.write(txt_pl_prev.format(diff_value, diff_percent))
+                st.divider()
     except Exception as e:
         st.write(str(e))
 
@@ -144,30 +170,149 @@ def display_consolidation():
         txt = "Total profit/ loss percentage: {:.2f}"
         st.write(txt.format((st.session_state["total_current_value_equity"]-st.session_state["total_invested_value_equity"])/st.session_state["total_invested_value_equity"]*100.0))
 
+@st.dialog('Adding stock...')
+def add_stock():
+    # read portfolio file
+    with open("data/portfolio.json", "r") as file:
+        portfolio_data = json.load(file)
+    new_dict = {}
+    with st.container(border=True):
+
+        add_stock_cols = st.columns(2)
+        with add_stock_cols[0]:
+            st.write("Enter stock name: ")
+        with add_stock_cols[1]:
+            stock_name = st.text_input("Stock name:", label_visibility="collapsed")
+
+        add_stock_cols = st.columns(2)
+        with add_stock_cols[0]:
+            st.write("No. of shares bought: ")
+        with add_stock_cols[1]:
+            new_dict["no. of shares"] = st.number_input(label='n_stocks', label_visibility='collapsed', step=1)
+
+        add_stock_cols = st.columns(2)
+        with add_stock_cols[0]:
+            st.write("Invested price: ")
+        with add_stock_cols[1]:
+            new_dict["invested price"] = st.number_input(label='n_stocks', label_visibility='collapsed', format="%.2f")
+
+        if st.button("Add stock!"):
+            portfolio_data['equity'][stock_name] = new_dict
+            try:
+                portfolio_data['equity'][stock_name]["current price"] = yfinance.Ticker(ticker=stock_name).info['currentPrice']
+            except:
+                portfolio_data['equity'][stock_name]["current price"] = portfolio_data['equity'][stock_name]["invested price"]
+            try:
+                portfolio_data['equity'][stock_name]["previous close"] = yfinance.Ticker(ticker=stock_name).info['previousClose']
+            except:
+                portfolio_data['equity'][stock_name]["previous close"] = portfolio_data['equity'][stock_name]["invested price"]
+
+            stocks_name_list = list(portfolio_data["equity"].keys())
+            stocks_name_list.sort()
+            sorted_equities_dict = {stock: portfolio_data['equity'][stock] for stock in stocks_name_list}
+            portfolio_data['equity'] = sorted_equities_dict
+            with open("data/portfolio.json", "w") as file:
+                json.dump(portfolio_data, file, indent=4)
+            st.rerun()
+
+@st.dialog('Editing details...')
+def edit_details():
+    # read portfolio file
+    with open("data/portfolio.json", "r") as file:
+        portfolio_data = json.load(file)
+    stocks_name_list = list(portfolio_data["equity"].keys())
+    with st.container(border=True):
+        edit_details_cols = st.columns(2)
+        with edit_details_cols[0]:
+            st.write("Stock name: ")
+        with edit_details_cols[1]:
+            stock_selected = st.selectbox("Stock name:", stocks_name_list, label_visibility="collapsed")
+
+        edit_details_cols = st.columns(2)
+        with edit_details_cols[0]:
+            st.write("No. of shares: ")
+        with edit_details_cols[1]:
+            new_quantity = st.number_input(value=portfolio_data['equity'][stock_selected]["no. of shares"], 
+                                        label='n_stocks', label_visibility='collapsed')
+            
+        edit_details_cols = st.columns(2)
+        with edit_details_cols[0]:
+            st.write("Invested price: ")
+        with edit_details_cols[1]:
+            new_current_price = st.number_input(value=portfolio_data['equity'][stock_selected]["invested price"], 
+                                                label='n_stocks', label_visibility='collapsed')
+            
+        if st.button("Save changes!"):
+            portfolio_data['equity'][stock_selected]["no. of shares"] = new_quantity
+            portfolio_data['equity'][stock_selected]["invested price"] = new_current_price
+            with open("data/portfolio.json", "w") as file:
+                json.dump(portfolio_data, file, indent=4)
+            st.rerun()
+    return
+
+@st.dialog('Removing stock...')
+def remove_stock():
+    # read portfolio file
+    with open("data/portfolio.json", "r") as file:
+        portfolio_data = json.load(file)
+    stocks_name_list = list(portfolio_data["equity"].keys())
+
+    with st.container(border=True):
+        remove_stock_cols = st.columns(2)
+        with remove_stock_cols[0]:
+            st.write("Stock name: ")
+        with remove_stock_cols[1]:
+            stock_selected = st.selectbox("Stock name:", stocks_name_list, label_visibility="collapsed")
+
+        st.write(f"Do you want to remove **:red[{stock_selected}]**?")
+
+        remove_stock_cols = st.columns(2)
+        with remove_stock_cols[0]:    
+            if st.button("Yes! Remove selected."):
+                del portfolio_data['equity'][stock_selected]
+                with open("data/portfolio.json", "w") as file:
+                    json.dump(portfolio_data, file, indent=4)
+                st.rerun()
+        with remove_stock_cols[1]:
+            if st.button("No! Close dialog."):
+                st.rerun()
+    return
+
 ########################################################################################################################
 
 st.set_page_config(layout="wide")
 
 st.title('Personal Finance - Equities')
 
-refresh_button_placeholder = st.empty()
+controls_placeholder = st.empty()
 column_header_container = st.empty()
 stock_values_container = st.empty()
 consolidated_container = st.empty()
 
 initialize_session_state()
 
-with refresh_button_placeholder:
-    if st.button('Refresh values'):
-        refresh_equity_values()
-
+with controls_placeholder.container(border=True):
+    controls_cols = st.columns([0.1,0.08,0.08,0.6])
+    with controls_cols[0]:
+        if st.button('Refresh values'):
+            refresh_equity_values()
+    with controls_cols[1]:
+        if st.button('Add stock'):
+            add_stock()
+    with controls_cols[2]:
+        if st.button('Edit details'):
+            edit_details()
+    with controls_cols[3]:
+        if st.button('Remove stock'):
+            remove_stock()
+            
 display_column_header()
 
 load_equity_page()
 
 display_consolidation()    
 
-# yfinance.Ticker(ticker='goldbees.ns').info
+yfinance.Ticker(ticker='cipla.ns').info
 
 # ticker = st.text_input(label='ticker', value='infy')
 # stock_info = yfinance.Ticker(ticker=ticker).info
